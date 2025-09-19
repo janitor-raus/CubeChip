99b053ceb709f605602de168fc5727fc585e58d9
Janitor Raus <11655762+janitor-raus@users.noreply.github.com>
2025-09-19 22:05:56 +0300

    Remove space from _rgb operator declaration, clang complains.

--- include/utilities/ColorOps.hpp
+++ include/utilities/ColorOps.hpp
@@ -213,7 +213,7 @@ struct alignas(4) RGBA {
 };
 
 // Shifts an (A)RGB color to RGBA color
-constexpr RGBA operator"" _rgb(unsigned long long value) noexcept
+constexpr RGBA operator""_rgb(unsigned long long value) noexcept
 	{ return RGBA::Packed(value << 8); }
 
 
