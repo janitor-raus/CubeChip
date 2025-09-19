8aca0d3693296ea0dd3fbc8d7e798bee3358d56c
Janitor Raus <11655762+janitor-raus@users.noreply.github.com>
2025-09-19 22:07:53 +0300

    SDL already checks for nullptr when freeing resources, so it's extraneous to do it a second time.

--- include/utilities/LifetimeWrapperSDL.cpp
+++ include/utilities/LifetimeWrapperSDL.cpp
@@ -10,10 +10,10 @@
 
 /*==================================================================*/
 
-void SDL_Deleter<SDL_Window>     ::operator()(SDL_Window*      ptr) const noexcept { if (ptr) { SDL_DestroyWindow(ptr); } }
-void SDL_Deleter<SDL_Renderer>   ::operator()(SDL_Renderer*    ptr) const noexcept { if (ptr) { SDL_DestroyRenderer(ptr); } }
-void SDL_Deleter<SDL_Texture>    ::operator()(SDL_Texture*     ptr) const noexcept { if (ptr) { SDL_DestroyTexture(ptr); } }
-void SDL_Deleter<SDL_AudioStream>::operator()(SDL_AudioStream* ptr) const noexcept { if (ptr) { SDL_DestroyAudioStream(ptr); } }
-void SDL_Deleter<unsigned>       ::operator()(unsigned*        ptr) const noexcept { if (ptr) { SDL_free(ptr); } }
-void SDL_Deleter<char>           ::operator()(char*            ptr) const noexcept { if (ptr) { SDL_free(ptr); } }
-void SDL_Deleter<const char>     ::operator()(const char*      ptr) const noexcept { if (ptr) { SDL_free(const_cast<char*>(ptr)); } }
+void SDL_Deleter<SDL_Window>     ::operator()(SDL_Window*      ptr) const noexcept { SDL_DestroyWindow(ptr); }
+void SDL_Deleter<SDL_Renderer>   ::operator()(SDL_Renderer*    ptr) const noexcept { SDL_DestroyRenderer(ptr); }
+void SDL_Deleter<SDL_Texture>    ::operator()(SDL_Texture*     ptr) const noexcept { SDL_DestroyTexture(ptr); }
+void SDL_Deleter<SDL_AudioStream>::operator()(SDL_AudioStream* ptr) const noexcept { SDL_DestroyAudioStream(ptr); }
+void SDL_Deleter<unsigned>       ::operator()(unsigned*        ptr) const noexcept { SDL_free(ptr); }
+void SDL_Deleter<char>           ::operator()(char*            ptr) const noexcept { SDL_free(ptr); }
+void SDL_Deleter<const char>     ::operator()(const char*      ptr) const noexcept { SDL_free(const_cast<char*>(ptr)); }
