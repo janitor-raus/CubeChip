cd6c6f93552bbb84b4202add4826b329774bf63a
Janitor Raus <11655762+janitor-raus@users.noreply.github.com>
2026-01-04 15:30:38 +0200

    Harden/refine thread_affinity namespace methods, and add more notes to the doxy. The Manager class is now effectively no-op outside of Windows/Linux.

--- include/utilities/ThreadAffinity.cpp
+++ include/utilities/ThreadAffinity.cpp
@@ -64,38 +64,52 @@ unsigned thread_affinity::get_current_core() noexcept {
 
 #if defined(_WIN32)
 	bool thread_affinity::set_affinity(unsigned long long affinity_mask, void* thread_handle) noexcept {
-		void* chosen_thread{ thread_handle ? thread_handle : GetCurrentThread() };
-		return SetThreadAffinityMask(static_cast<HANDLE>(chosen_thread), affinity_mask & ((1ull << get_logical_core_count()) - 1));
+		const auto cpu_count = get_logical_core_count();
+
+		const auto valid_mask = cpu_count >= 64 ? affinity_mask
+			: affinity_mask & ((1 << cpu_count) - 1);
+
+		if (valid_mask == 0x0) { return false; }
+
+		auto thread = thread_handle ? static_cast<HANDLE>(thread_handle) : GetCurrentThread();
+		return SetThreadAffinityMask(thread, static_cast<DWORD_PTR>(valid_mask)) != 0;
 	}
 #elif defined(__linux__)
 	bool thread_affinity::set_affinity(unsigned long long affinity_mask, void* thread_handle) noexcept {
-		if ((affinity_mask & ((1ull << get_logical_core_count()) - 1)) == 0x0) { return false; }
+		const auto cpu_count = std::min(get_logical_core_count(),
+			static_cast<unsigned>(CPU_SETSIZE));
 
-		cpu_set_t cpu_aff; CPU_ZERO(&cpu_aff);
+		const auto valid_mask = cpu_count >= 64 ? affinity_mask
+			: affinity_mask & ((1ull << cpu_count) - 1);
 
-		for (auto i{ 0 }; i < 64; ++i) {
-			if (affinity_mask & (1ull << i))
-				{ CPU_SET(i, &cpu_aff); }
+		if (valid_mask == 0) { return false; }
+
+		cpu_set_t set; CPU_ZERO(&set);
+
+		for (auto i = 0; i < cpu_count; ++i) {
+			if (valid_mask & (1ull << i)) { CPU_SET(i, &set); }
 		}
 
-		auto chosen_thread{ thread_handle ? *static_cast<pthread_t*>(thread_handle) : pthread_self() };
-		return pthread_setaffinity_np(chosen_thread, sizeof(cpu_set_t), &cpu_aff) == 0;
+		auto thread = thread_handle ? *static_cast<pthread_t*>(thread_handle) : pthread_self();
+		return pthread_setaffinity_np(thread, sizeof(set), &set) == 0;
 	}
 #elif defined(__APPLE__)
 	bool thread_affinity::set_affinity(unsigned long long affinity_tag, void* thread_handle) noexcept {
-		auto chosen_tag{ static_cast<integer_t>(affinity_tag) };
-		auto chosen_thread{ thread_handle ? *static_cast<pthread_t*>(thread_handle) : pthread_self() };
+		auto tag = static_cast<integer_t>(affinity_tag);
+		auto thread = thread_handle ? *static_cast<pthread_t*>(thread_handle) : pthread_self();
 
 		return thread_policy_set(
-			pthread_mach_thread_np(chosen_thread), THREAD_AFFINITY_POLICY,
-			&chosen_tag, THREAD_AFFINITY_POLICY_COUNT) == KERN_SUCCESS;
+			pthread_mach_thread_np(thread), THREAD_AFFINITY_POLICY,
+			&tag, THREAD_AFFINITY_POLICY_COUNT) == KERN_SUCCESS;
 	}
+#else
+	bool thread_affinity::set_affinity(unsigned long long, void*) noexcept { return false; }
 #endif
 
 /*==================================================================*/
 
 static auto get_core_group() noexcept {
-	const auto core_affinity_group{ thread_affinity::get_current_core() & ~1u };
+	const auto core_affinity_group = thread_affinity::get_current_core() & ~1u;
 	return 1ull << core_affinity_group | 1ull << (core_affinity_group + 1);
 }
 
@@ -103,26 +117,30 @@ thread_affinity::Manager::Manager(
 	unsigned           cooldown_p,
 	unsigned long long avoid_mask
 ) noexcept
-	: timestamp{ -Millis::now() }
-	, cooldown_p{ cooldown_p * 1000 }
-	, avoid_mask{ avoid_mask }
+	: timestamp(-Millis::now())
+	, cooldown_p(cooldown_p * 1000)
+	, avoid_mask(avoid_mask )
 {}
 
-bool thread_affinity::Manager::refresh_affinity() noexcept {
-	if (is_thread_pinned) {
-		if (Millis::since(timestamp) >= cooldown_p) {
-			set_affinity(~avoid_mask);
-			is_thread_pinned = false;
+#if defined(_WIN32) || defined(__linux__)
+	bool thread_affinity::Manager::refresh_affinity() noexcept {
+		if (is_thread_pinned) {
+			if (Millis::since(timestamp) >= cooldown_p) {
+				set_affinity(~avoid_mask);
+				is_thread_pinned = false;
+			}
+		} else {
+			auto this_group = ::get_core_group();
+			if (this_group != last_group) {
+				last_group = this_group;
+				timestamp  = Millis::now();
+				set_affinity(this_group);
+				is_thread_pinned = true;
+				return true;
+			}
 		}
-	} else {
-		auto this_group{ ::get_core_group() };
-		if (this_group != last_group) {
-			last_group = this_group;
-			timestamp = Millis::now();
-			set_affinity(this_group);
-			is_thread_pinned = true;
-			return true;
-		}
+		return false;
 	}
-	return false;
-}
+#else
+	bool thread_affinity::Manager::refresh_affinity() noexcept { return false; }
+#endif
--- include/utilities/ThreadAffinity.hpp
+++ include/utilities/ThreadAffinity.hpp
@@ -11,18 +11,21 @@
 namespace thread_affinity {
 	/**
 	 * @brief Attempt to grab the current process ID. Defaults to 0.
+	 * @warning on Web, this will always return 0.
 	 * @return Process ID number.
 	 */
 	unsigned get_process_id() noexcept;
 
 	/**
 	 * @brief Guesstimate of amount of logical cores the system has. Defaults to 1.
+	 * @warning On Linux/MacOS, hardware concurrency is used as fallback. On Web, defaults to 1.
 	 * @return Amount of logical cores.
 	 */
 	unsigned get_logical_core_count() noexcept;
 
 	/**
 	 * @brief Guesstimate of which logical processor core the current thread runs on. Defaults to 0.
+	 * @warning On MacOS/Web, this will always return 0 due to platform limitations.
 	 * @return Processor core ID number.
 	 */
 	unsigned get_current_core() noexcept;
@@ -30,8 +33,10 @@ namespace thread_affinity {
 #if defined(_WIN32)
 	/**
 	 * @brief Sets thread affinity to desired logical cores. Will ignore invalid affinity masks safely.
+	 * @warning Only valid up to 64 logical cores (for now). On MacOS, the mask is used as a tag instead.
+	 *       On web, this is a no-op.
 	 * @param[in] affinity_mask :: Bitwise mask to control which logical cores are eligible.
-	 * @param[in] thread_handle :: Thread handle pointer to (optionally) target a different thread, otherwise pick own thread.
+	 * @param[in] thread_handle :: Thread handle pointer to thread, if null, picks caller thread.
 	 * @return True if successful, false otherwise.
 	 */
 	bool set_affinity(unsigned long long affinity_mask, void* thread_handle = nullptr) noexcept;
@@ -66,6 +71,9 @@ namespace thread_affinity {
 	 * every time the system scheduler migrates the thread to a different core group, effective
 	 * for a defined cooldown period. An affinity mask can be provided to avoid specific cores.
 	 *
+	 * @warning This class only works for Windows/Linux due to platform limitations. You should
+	 *          NOT rely on the return value of `refresh_affinity()` for important tasks!
+	 *
 	 * Usage:
 	 * - Construct a Manager object (ideally TLS) at the start of a thread or section of code.
 	 * - Call `refresh_affinity()` periodically to enforce the pinning policy.
@@ -74,7 +82,7 @@ namespace thread_affinity {
 	 * @code
 	 * thread_affinity::Manager ag(100, 0b11); // 100 sec cooldown, avoid first two cores
 	 * if (ag.refresh_affinity()) {
-	 *     // thread was newly pinned, do something special
+	 *     // thread was pinned, do something special
 	 * }
 	 * @endcode
 	 */
@@ -82,7 +90,7 @@ namespace thread_affinity {
 		long long timestamp{};
 		unsigned  cooldown_p{};
 
-		bool is_thread_pinned{ true };
+		bool is_thread_pinned = true;
 
 		unsigned long long avoid_mask{};
 		unsigned long long last_group{};
