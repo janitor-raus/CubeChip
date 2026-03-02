456daac037af323ed62dc82623c00bd7f07f1d73
Janitor Raus <11655762+janitor-raus@users.noreply.github.com>
2026-03-02 14:08:31 +0200

    Undo cxxopts update, fetchcontent don't like it on github cli.

--- cmake/cxxopts.cmake
+++ cmake/cxxopts.cmake
@@ -1,6 +1,6 @@
 
 fetch_and_vendor(
     "https://github.com/jarro2783/cxxopts.git"
-    "068f6c582c592eda5364f1b18d0ccb1bb4754ad2" TRUE
+    "v3.3.1" TRUE
     "${PROJECT_VENDOR_DIR}/cxxopts" TRUE
 )
