05780289a757d56a8862b6d6e4f0e14489d15c7c
Janitor Raus <11655762+janitor-raus@users.noreply.github.com>
2026-06-06 16:13:40 +0300

    Simplify AudioDevice api by hardcoding the float format and removing the internal Stream struct in favor of only managing a single stream per logical device, not multiple.

--- include/components/AudioDevice.cpp
+++ include/components/AudioDevice.cpp
@@ -15,84 +15,45 @@
 
 /*==================================================================*/
 
-auto AudioDevice::insert_audio_stream(StreamID stream_id, SDL_AudioStream* device_ptr) noexcept -> Stream* {
-	if (auto slot = at(stream_id)) {
-		blog.warn("Audio stream with ID '{}' already exists!", stream_id);
-		*slot = Stream(device_ptr);
-		return slot;
-	} else {
-		auto [it, inserted] = m_audio_streams.try_emplace(stream_id, device_ptr);
-		if (inserted) {
-			return &it->second;
-		} else {
-			blog.error("Failed to insert audio stream with ID '{}'!", stream_id);
-			(void) sdl::make_unique<SDL_AudioStream>(device_ptr); // auto cleanup!
-			return nullptr;
-		}
-	}
-}
-
-void AudioDevice::add_playback_stream(StreamID stream_id, signed freq, signed channels) noexcept {
-	if (auto* device_ptr = SDL_OpenAudioDeviceStream(
-		SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK,
+void AudioDevice::init_stream(signed freq, signed channels, bool recording_device) noexcept {
+	if (auto* device_ptr = SDL_OpenAudioDeviceStream(recording_device
+		? SDL_AUDIO_DEVICE_DEFAULT_RECORDING
+		: SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK,
 		nullptr, nullptr, nullptr
 	)) {
 		const bool new_freq = freq > 0;
 		const bool new_channels = channels >= 1 && channels <= 8;
 
-		if (auto stream_ptr = insert_audio_stream(stream_id, device_ptr)) {
-			stream_ptr->set_spec(new_freq ? freq : 0, new_channels ? channels : 0);
-		}
+		m_stream = device_ptr;
+		set_spec(new_freq ? freq : 0, new_channels ? channels : 0);
 	} else {
-		blog.error("Failed to open audio playback stream: {}", SDL_GetError());
-	}
-}
-
-void AudioDevice::add_recording_stream(StreamID stream_id, signed freq, signed channels) noexcept {
-	if (auto* device_ptr = SDL_OpenAudioDeviceStream(
-		SDL_AUDIO_DEVICE_DEFAULT_RECORDING,
-		nullptr, nullptr, nullptr
-	)) {
-		const bool new_freq = freq > 0;
-		const bool new_channels = channels >= 1 && channels <= 8;
-
-		if (auto stream_ptr = insert_audio_stream(stream_id, device_ptr)) {
-			stream_ptr->set_spec(new_freq ? freq : 0, new_channels ? channels : 0);
-		}
-	} else {
-		blog.error("Failed to open audio recording stream: {}", SDL_GetError());
+		blog.error("Failed to open audio stream: {}", SDL_GetError());
 	}
 }
 
 /*==================================================================*/
 
-void AudioDevice::pause_all_streams() noexcept {
-	for (auto& stream : m_audio_streams) {
-		SDL_PauseAudioStreamDevice(stream.second);
-	}
-}
-
-void AudioDevice::resume_all_streams() noexcept {
-	for (auto& stream : m_audio_streams) {
-		SDL_ResumeAudioStreamDevice(stream.second);
-	}
-}
-
-/*==================================================================*/
-
-void AudioDevice::Stream::update_cached_spec() noexcept {
+void AudioDevice::update_cached_spec() noexcept {
 	SDL_AudioSpec actual;
 
 	if (is_playback()) {
-		SDL_GetAudioStreamFormat(m_ptr, &actual, nullptr);
+		SDL_GetAudioStreamFormat(m_stream, &actual, nullptr);
 	} else {
-		SDL_GetAudioStreamFormat(m_ptr, nullptr, &actual);
+		SDL_GetAudioStreamFormat(m_stream, nullptr, &actual);
+	}
+
+	if (actual.format == SDL_AUDIO_UNKNOWN) {
+		// If the stream fails to provide a valid spec, all fields are zeroed
+		// and this allows us to know implicitly that something went wrong.
+		blog.warn("Failed to fetch audio stream spec (the device might "
+			"have been lost, consider re-creating it): {}", SDL_GetError());
+		m_stream = nullptr; actual.freq = 0; actual.channels = 0;
 	}
 
 	m_freq = actual.freq; m_channels = actual.channels;
 }
 
-bool AudioDevice::Stream::set_spec(signed freq, signed channels) noexcept {
+bool AudioDevice::set_spec(signed freq, signed channels) noexcept {
 	const bool needs_default_freq = freq <= 0;
 	const bool needs_default_channels = channels < 1 || channels > 8;
 
@@ -108,19 +69,21 @@ bool AudioDevice::Stream::set_spec(signed freq, signed channels) noexcept {
 	if (new_freq == m_freq && new_channels == m_channels) { return true; }
 	const SDL_AudioSpec spec{ SDL_AUDIO_F32, new_channels, new_freq };
 
-	if (SDL_SetAudioStreamFormat(m_ptr, &spec, &spec)) {
+	if (SDL_SetAudioStreamFormat(m_stream, &spec, &spec)) {
 		update_cached_spec(); m_accumulator = 0;
 		return true;
 	} else {
-		blog.warn("Failed to update audio stream spec: {}", SDL_GetError());
+		blog.warn("Failed to update audio stream spec (the device might "
+			"have been lost, consider re-creating it): {}", SDL_GetError());
+		m_stream = nullptr; m_freq = 0; m_channels = 0;
 		return false;
 	}
 }
 
-bool AudioDevice::Stream::set_freq_ratio(float ratio) noexcept {
+bool AudioDevice::set_freq_ratio(float ratio) noexcept {
 	ratio = std::clamp(ratio, 0.01f, 100.0f);
-	if (SDL_SetAudioStreamFrequencyRatio(m_ptr, ratio)) {
-		m_last_freq_ratio = ratio;
+	if (SDL_SetAudioStreamFrequencyRatio(m_stream, ratio)) {
+		m_freq_ratio = ratio;
 		return true;
 	} else {
 		blog.warn("Failed to set audio stream frequency ratio: {}", SDL_GetError());
@@ -128,33 +91,33 @@ bool AudioDevice::Stream::set_freq_ratio(float ratio) noexcept {
 	}
 }
 
-bool AudioDevice::Stream::is_paused() const noexcept {
+bool AudioDevice::is_paused() const noexcept {
 	// We're gating with the device's ID first, as it allows us to
 	// check for an orphaned stream (one whose device was closed)
 	// and thus implicitly report "paused" to gate other operations.
-	const auto device_id = SDL_GetAudioStreamDevice(m_ptr);
+	const auto device_id = SDL_GetAudioStreamDevice(m_stream);
 	return device_id ? SDL_AudioDevicePaused(device_id) : true;
 }
 
-bool AudioDevice::Stream::is_playback() const noexcept {
-	return SDL_IsAudioDevicePlayback(SDL_GetAudioStreamDevice(m_ptr));
+bool AudioDevice::is_playback() const noexcept {
+	return SDL_IsAudioDevicePlayback(SDL_GetAudioStreamDevice(m_stream));
 }
 
-float AudioDevice::Stream::get_samples_per_frame(float target_framerate) const noexcept {
+float AudioDevice::get_samples_per_frame(float target_framerate) const noexcept {
 	if (target_framerate < 0.1f) { return 0.0f; }
-	return m_freq * m_last_freq_ratio / target_framerate * m_channels;
+	return m_freq * m_freq_ratio / target_framerate * m_channels;
 }
 
-auto AudioDevice::Stream::next_frame_sample_count(float target_framerate) noexcept -> std::size_t {
+auto AudioDevice::next_frame_sample_count(float target_framerate) noexcept -> std::size_t {
 	if (target_framerate < 0.1f) { return 0; }
 
-	if (target_framerate != m_last_target_framerate) {
-		m_last_target_framerate = target_framerate;
+	if (target_framerate != m_target_framerate) {
+		m_target_framerate = target_framerate;
 		m_accumulator = 0;
 	}
 
 	static constexpr auto c_scale_factor = 1ull << 24;
-	::assign_cast_add(m_accumulator, m_freq * m_last_freq_ratio
+	::assign_cast_add(m_accumulator, m_freq * m_freq_ratio
 		/ target_framerate * c_scale_factor);
 	const auto sample_amount = m_accumulator >> 24;
 	::assign_cast_and(m_accumulator, c_scale_factor - 1);
@@ -162,32 +125,31 @@ auto AudioDevice::Stream::next_frame_sample_count(float target_framerate) noexce
 	return sample_amount * m_channels;
 }
 
-void AudioDevice::Stream::pause() noexcept {
-	SDL_PauseAudioStreamDevice(m_ptr);
+void AudioDevice::pause() noexcept {
+	SDL_PauseAudioStreamDevice(m_stream);
 }
 
-void AudioDevice::Stream::resume() noexcept {
-	SDL_ResumeAudioStreamDevice(m_ptr);
+void AudioDevice::resume() noexcept {
+	SDL_ResumeAudioStreamDevice(m_stream);
 }
 
-float AudioDevice::Stream::get_gain() const noexcept {
-	return SDL_GetAudioStreamGain(m_ptr);
+float AudioDevice::get_gain() const noexcept {
+	return SDL_GetAudioStreamGain(m_stream);
 }
 
-void AudioDevice::Stream::set_gain(float new_gain) noexcept {
-	SDL_SetAudioStreamGain(m_ptr, std::clamp(new_gain, 0.0f, 2.0f));
+void AudioDevice::set_gain(float new_gain) noexcept {
+	SDL_SetAudioStreamGain(m_stream, std::clamp(new_gain, 0.0f, 2.0f));
 }
 
-void AudioDevice::Stream::add_gain(float add_gain) noexcept {
+void AudioDevice::add_gain(float add_gain) noexcept {
 	set_gain(get_gain() + add_gain);
 }
 
-void AudioDevice::Stream::push_raw_audio_data(void* sample_data,
-	std::size_t buffer_size, std::size_t sample_size
-) noexcept {
-	if (is_paused() || buffer_size == 0) { return; }
+void AudioDevice::push_raw_audio_data(const float* sample_data, std::size_t sample_count) noexcept {
+	if (is_paused() || sample_count == 0) { return; }
 
-	SDL_SetAudioDeviceGain(SDL_GetAudioStreamDevice(m_ptr),
+	SDL_SetAudioDeviceGain(SDL_GetAudioStreamDevice(m_stream),
 		GlobalAudioBase::get_final_volume());
-	SDL_PutAudioStreamData(m_ptr, sample_data, signed(buffer_size * sample_size));
+	SDL_PutAudioStreamData(m_stream, sample_data,
+		signed(sample_count * sizeof(float)));
 }
--- include/components/AudioDevice.hpp
+++ include/components/AudioDevice.hpp
@@ -6,138 +6,104 @@
 
 #pragma once
 
-#include <unordered_map>
-
 #include "Concepts.hpp"
 #include "LifetimeWrapperSDL.hpp"
 
 /*==================================================================*/
 
 class AudioDevice {
-	using StreamID = signed int;
+	SDL_Unique<SDL_AudioStream> m_stream;
+	signed m_freq = 0, m_channels = 0;
+	float m_target_framerate = 0.0f;
+	float m_freq_ratio = 1.0f;
+	unsigned long long m_accumulator = 0;
+
+	void update_cached_spec() noexcept;
 
 public:
-	class Stream {
-		SDL_Unique<SDL_AudioStream> m_ptr;
-		signed m_freq{}, m_channels{};
-		float m_last_target_framerate{};
-		float m_last_freq_ratio = 1.0f;
-		unsigned long long m_accumulator{};
+	// Sets the stream's audio spec. Zero/invalid values fall back to the physical
+	// device's default respectively. If a change in spec was applied, the internal
+	// accumulator for next_frame_sample_count() will be reset to prevent drift.
+	bool set_spec(signed frequency = 0, signed channels = 0) noexcept;
 
-	public:
-		Stream(SDL_AudioStream* ptr) noexcept
-			: m_ptr(ptr) { update_cached_spec(); }
+	auto get_freq()      const noexcept { return m_freq; }
+	bool set_freq(signed freq) noexcept { return set_spec(freq, m_channels); }
 
-	private:
-		void update_cached_spec() noexcept;
+	auto get_freq_ratio() const noexcept { return m_freq_ratio; }
+	bool set_freq_ratio(float ratio) noexcept;
 
-	public:
-		// Sets the stream's audio spec. Zero/invalid values fall back to the physical
-		// device's default respectively. If a change in spec was applied, the internal
-		// accumulator for next_frame_sample_count() will be reset to prevent drift.
-		bool set_spec(signed frequency = 0, signed channels = 0) noexcept;
+	auto get_channels()          const noexcept { return m_channels; }
+	bool set_channels(signed channels) noexcept { return set_spec(m_freq, channels); }
 
-		auto get_freq()      const noexcept { return m_freq; }
-		bool set_freq(signed freq) noexcept { return set_spec(freq, m_channels); }
+	bool is_paused()   const noexcept;
+	bool is_playback() const noexcept;
 
-		auto get_freq_ratio() const noexcept { return m_last_freq_ratio; }
-		bool set_freq_ratio(float ratio) noexcept;
+	float get_samples_per_frame(float framerate) const noexcept;
 
-		auto get_channels()          const noexcept { return m_channels; }
-		bool set_channels(signed channels) noexcept { return set_spec(m_freq, channels); }
+	/**
+	 * @brief Calculates the number of audio samples needed for the next frame, based on
+	 *        the stream's frequency and the given framerate. Internally uses an
+	 *        accumulator to handle fractional sample counts across frames, to handle
+	 *        cases where the framerate doesn't perfectly divide the frequency.
+	 * @note The internal accumulator progress will be reset if the framerate given
+	 *       has changed since the last call of this function to avoid drift.
+	 * @param framerate :: The current (expected) framerate of a system.
+	 * @return The number of audio samples needed for your next frame, multiplied by the
+	 *         number of channels. If the given framerate is less than 1.0, returns 0.
+	 */
+	[[nodiscard]]
+	auto next_frame_sample_count(float framerate) noexcept -> std::size_t;
 
-		bool is_paused()   const noexcept;
-		bool is_playback() const noexcept;
+	void pause() noexcept;
+	void resume() noexcept;
 
-		float get_samples_per_frame(float framerate) const noexcept;
+	float get_gain() const noexcept;
+	void  set_gain(float gain) noexcept;
+	void  add_gain(float gain) noexcept;
 
-		/**
-		 * @brief Calculates the number of audio samples needed for the next frame, based on
-		 *        the stream's frequency and the given framerate. Internally uses an
-		 *        accumulator to handle fractional sample counts across frames, to handle
-		 *        cases where the framerate doesn't perfectly divide the frequency.
-		 * @note The internal accumulator progress will be reset if the framerate given
-		 *       has changed since the last call of this function to avoid drift.
-		 * @param framerate :: The current (expected) framerate of a system.
-		 * @return The number of audio samples needed for your next frame, multiplied by the
-		 *         number of channels. If the given framerate is less than 1.0, returns 0.
-		 */
-		[[nodiscard]]
-		auto next_frame_sample_count(float framerate) noexcept -> std::size_t;
+	operator SDL_AudioStream*() const noexcept { return m_stream.get(); }
 
-		void pause() noexcept;
-		void resume() noexcept;
+	void push_raw_audio_data(const float* sample_data, std::size_t sample_count) noexcept;
 
-		float get_gain() const noexcept;
-		void  set_gain(float gain) noexcept;
-		void  add_gain(float gain) noexcept;
+	/**
+	 * @brief Pushes buffer of audio samples to SDL device/stream.
+	 * @param[in] sample_data :: pointer to audio samples buffer.
+	 * @param[in] sample_count :: size of buffer in bytes.
+	 */
+	template <IsPlainOldData T>
+	void push_audio_data(const float* sample_data, std::size_t sample_count) noexcept {
+		push_raw_audio_data(sample_data, sample_count);
+	}
 
-		operator SDL_AudioStream*() const noexcept { return m_ptr.get(); }
+	/**
+	 * @brief Pushes buffer of audio samples to SDL device/stream.
+	 * @param[in] samples_buffer :: audio samples buffer (C style).
+	 */
+	template <std::size_t N>
+	void push_audio_data(const float(&samples_buffer)[N]) noexcept {
+		push_raw_audio_data(samples_buffer, N);
+	}
 
-		void push_raw_audio_data(void* sample_data, std::size_t buffer_size, std::size_t sample_size) noexcept;
-
-		/**
-		 * @brief Pushes buffer of audio samples to SDL device/stream.
-		 * @param[in] index :: the device/stream to push audio to.
-		 * @param[in] sample_data :: pointer to audio samples buffer.
-		 * @param[in] buffer_size :: size of buffer in bytes.
-		 */
-		template <IsPlainOldData T>
-		void push_audio_data(T* sample_data, std::size_t buffer_size) noexcept {
-			push_raw_audio_data(sample_data, buffer_size, sizeof(T));
-		}
-
-		/**
-		 * @brief Pushes buffer of audio samples to SDL device/stream.
-		 * @param[in] index :: the device/stream to push audio to.
-		 * @param[in] samples_buffer :: audio samples buffer (C style).
-		 */
-		template <IsPlainOldData T, std::size_t N>
-		void push_audio_data(T(&samples_buffer)[N]) noexcept {
-			push_raw_audio_data(samples_buffer, N, sizeof(T));
-		}
-
-		/**
-		 * @brief Pushes buffer of audio samples to SDL device/stream.
-		 * @param[in] index :: the device/stream to push audio to.
-		 * @param[in] samples_buffer :: audio samples buffer (C++ style).
-		 */
-		template <IsContiguousContainer T> requires(IsPlainOldData<ValueType<T>>)
-		void push_audio_data(T& samples_buffer) noexcept {
-			push_raw_audio_data(std::data(samples_buffer), std::size(samples_buffer), sizeof(ValueType<T>));
-		}
-	};
-
-private:
-	std::unordered_map<StreamID, Stream>
-		m_audio_streams{};
+	/**
+	 * @brief Pushes buffer of audio samples to SDL device/stream.
+	 * @param[in] samples_buffer :: audio samples buffer (C++ style).
+	 */
+	template <IsContiguousContainer T> requires(std::same_as<ValueType<T>, float>)
+	void push_audio_data(const T& samples_buffer) noexcept {
+		push_raw_audio_data(std::data(samples_buffer), std::size(samples_buffer));
+	}
 
 public:
 	AudioDevice() noexcept = default;
 
+	operator bool() const noexcept { return bool(m_stream); }
+
 	AudioDevice(const AudioDevice&) = delete;
 	AudioDevice& operator=(const AudioDevice&) = delete;
-
-private:
-	auto insert_audio_stream(StreamID stream_id, SDL_AudioStream* stream_ptr) noexcept -> Stream*;
+	AudioDevice(AudioDevice&&) = delete;
+	AudioDevice& operator=(AudioDevice&&) = delete;
 
 public:
-	void add_playback_stream(StreamID stream_id, signed freq = 0, signed channels = 0) noexcept;
-	void add_recording_stream(StreamID stream_id, signed freq = 0, signed channels = 0) noexcept;
-
-	auto get_stream_count() const noexcept { return m_audio_streams.size(); }
-
-	void pause_all_streams()  noexcept;
-	void resume_all_streams() noexcept;
-
-	[[nodiscard]]
-	Stream& operator[](StreamID key) {
-		return m_audio_streams.at(key);
-	}
-
-	[[nodiscard]]
-	Stream* at(StreamID key) noexcept {
-		auto it = m_audio_streams.find(key);
-		return it != m_audio_streams.end() ? &it->second : nullptr;
-	}
+	void init_stream(signed freq = 0, signed channels = 0,
+		bool recording_device = false) noexcept;
 };
